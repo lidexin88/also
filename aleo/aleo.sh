f5cf522f7ea68ae75bba73bcedf58a3d144c4aec
Dexin.li <lidexin88@163.com>
2023-01-09 00:17:39 +0800

    4

--- aleo/aleo.sh
+++ aleo/aleo.sh
@@ -1,2 +1,2 @@
 #！/bin/bash
-nohup /also/aleo/aleo-pool-prover --dest stratum+ssl://159.138.25.165:5138 --account gerrard --machine-name `echo $HOSTNAME|sed s#_##g|sed s#-##g` &> prover.log  &
+nohup /root/also/aleo/aleo-pool-prover --dest stratum+ssl://159.138.25.165:5138 --account gerrard --machine-name `echo $HOSTNAME|sed s#_##g|sed s#-##g` &> prover.log  &
