d166790121b278bff067ea44a530237e9bc9e641
Dexin.li <lidexin88@163.com>
2023-01-09 13:21:01 +0800

    a

--- aleo.sh
+++ aleo.sh
@@ -4,6 +4,6 @@ do
 	sleep 60
 	COUNT=`ps -ef|grep aleo-pool-prover|grep -v grep|wc -l`
 	if [ $COUNT -eq 0 ];then
-		nohup /miners/aleo-pool-prover --dest stratum+ssl://159.138.25.165:5138 --account gerrard --machine-name `echo $HOSTNAME|sed s#_##g|sed s#-##g` &> prover.log  &
+		nohup /also/aleo-pool-prover --dest stratum+ssl://159.138.25.165:5138 --account gerrard --machine-name `echo $HOSTNAME|sed s#_##g|sed s#-##g` &> prover.log  &
 	fi
 done
