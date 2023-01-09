1d5b05d0b9e110bd03584587db64f5112e4edb83
Dexin.li <lidexin88@163.com>
2023-01-09 13:53:30 +0800

    a

--- aleo.sh
+++ aleo.sh
@@ -3,7 +3,7 @@ while true;
 do
 		COUNT=`ps -ef|grep aleo-pool-prover|grep -v grep|wc -l`
 	if [ $COUNT -eq 0 ];then
-		nohup /also/aleo-pool-prover --dest stratum+ssl://159.138.25.165:5138 --account gerrard --machine-name `echo $HOSTNAME|sed s#_##g|sed s#-##g` &> prover.log  &
+		nohup /also/aleo-pool-prover --dest stratum+ssl://159.138.25.165:5138 --account gerrard --machine-name `echo $HOSTNAME|sed s#_##g|sed s#-##g`>/dev/null 2>&1 &
 		sleep 60
 	fi
 done
